98d86224cebe15bf4edcc628a787be0ff00f7682
iivanov <iivanov+github.com@isilona.com>
2018-04-19 20:48:54 +0300

    commit simple wrong test to test travis  notification

---
+++ common/src/test/java/io/office360/common/security/Base64UnitTest.java
@@ -0,0 +1,39 @@
+package io.office360.common.security;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+
+public class Base64UnitTest {
+
+    private static final String BASIC_AUTHORIZATION = "Basic dGVzdEtleTp0ZXN0VmFs";
+
+    // tests
+
+    @Test
+    public final void whenAuthorizationHeaderIsDecoded_thenNoExceptions() {
+        SpringSecurityUtil.decodeAuthorizationKey(BASIC_AUTHORIZATION);
+    }
+
+    @Test
+    public final void whenAuthorizationHeaderIsDecoded_thenResponseNotNull() {
+        final Pair<String, String> authorizationKey = SpringSecurityUtil.decodeAuthorizationKey(BASIC_AUTHORIZATION);
+
+        assertNotNull(authorizationKey);
+        assertNotNull(authorizationKey.getLeft());
+        assertNotNull(authorizationKey.getRight());
+    }
+
+    @Test
+    public final void whenAuthorizationHeaderIsDecoded_thenResponseIsCorrect() {
+        final Pair<String, String> authorizationKey = SpringSecurityUtil.decodeAuthorizationKey(BASIC_AUTHORIZATION);
+
+        assertThat(authorizationKey.getLeft(), equalTo("testKeyc"));
+        assertThat(authorizationKey.getRight(), equalTo("testVal"));
+    }
+
+}
