4530390607364db1ab466be99da3dc6a72f650eb
iivanov <iivanov+github.com@isilona.com>
2018-06-19 22:02:52 +0300

    increase test coverage

--- common/src/test/java/io/office360/common/unit/web/RestPreconditionsUnitTest.java
+++ common/src/test/java/io/office360/common/unit/web/RestPreconditionsUnitTest.java
@@ -1,6 +1,8 @@
 package io.office360.common.unit.web;
 
 import io.office360.common.web.RestPreconditions;
+import io.office360.common.web.exception.Office360BadRequestException;
+import io.office360.common.web.exception.Office360ConflictException;
 import io.office360.common.web.exception.Office360ResourceNotFoundException;
 import org.junit.Assert;
 import org.junit.Test;
@@ -89,5 +91,37 @@ public class RestPreconditionsUnitTest {
                 reference);
     }
 
+    @Test(expected = Office360BadRequestException.class)
+    public void testValidatesThatMethodCheckRequestElementNotNull_ThrowsExceptionWithNullParameter() {
+
+        Object reference = null;
+        RestPreconditions.checkRequestElementNotNull(reference);
+
+    }
+
+    @Test
+    public void testValidatesThatMethodCheckRequestElementNotNull_ReturnsSameObject() {
+
+        Object reference = new Object();
+        Object result = RestPreconditions.checkRequestElementNotNull(reference);
+
+        Assert.assertSame("reference and result are not same", result,
+                reference);
+    }
+
+    @Test(expected = Office360ConflictException.class)
+    public void testValidatesThatMethodCheckRequestState_ThrowsExceptionWithFalseExpression() {
+
+        boolean expression = false;
+        RestPreconditions.checkRequestState(expression);
+
+    }
+
+    @Test
+    public void testValidatesThatMethodCheckRequestState_PassesWithFalseExpression() {
+
+        boolean expression = true;
+        RestPreconditions.checkRequestState(expression);
+    }
 
 }
