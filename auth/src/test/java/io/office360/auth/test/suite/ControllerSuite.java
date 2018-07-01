79bbfba73824b7d78ae9d212c7b197c107079215
iivanov <iivanov+github.com@isilona.com>
2018-07-01 07:57:02 +0300

    add controller test poc

--- auth/src/test/java/io/office360/auth/test/suite/ControllerSuite.java
+++ auth/src/test/java/io/office360/auth/test/suite/ControllerSuite.java
@@ -1,15 +1,15 @@
 package io.office360.auth.test.suite;
 
+import io.office360.auth.web.controller.PrivilegeControllerUnitTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+
 @RunWith(Suite.class)
 @SuiteClasses({
-        LiveSuite.class,
-        ServiceSuite.class,
-        ControllerSuite.class
+        PrivilegeControllerUnitTest.class
 })
-public final class AllSuite {
+public final class ControllerSuite {
     //
 }
