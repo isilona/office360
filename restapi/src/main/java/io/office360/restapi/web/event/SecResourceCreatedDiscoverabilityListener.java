1ac69e250762041c1969c1b86defbb7ccb0ff2c6
iivanov <iivanov+github.com@isilona.com>
2018-07-07 17:29:08 +0300

    fix Field injection is not recommended

--- restapi/src/main/java/io/office360/restapi/web/event/SecResourceCreatedDiscoverabilityListener.java
+++ restapi/src/main/java/io/office360/restapi/web/event/SecResourceCreatedDiscoverabilityListener.java
@@ -1,15 +1,18 @@
 package io.office360.restapi.web.event;
 
+import io.office360.common.web.IUriMapper;
 import io.office360.common.web.WebConstants;
 import io.office360.common.web.listeners.ResourceCreatedDiscoverabilityListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 // TODO: #1 Check usage
 @Component
 class SecResourceCreatedDiscoverabilityListener extends ResourceCreatedDiscoverabilityListener {
 
-    public SecResourceCreatedDiscoverabilityListener() {
-        super();
+    @Autowired
+    public SecResourceCreatedDiscoverabilityListener(IUriMapper uriMapper) {
+        super(uriMapper);
     }
 
     //
