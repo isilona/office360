b90f581ff70895031cf0954963a0da0ec485f86a
iivanov <iivanov+github.com@isilona.com>
2018-07-28 17:05:35 +0300

    fixing restapi test

--- restapi/src/test/java/oi/office360/restapi/service/ServiceSpringIntegrationTest.java
+++ restapi/src/test/java/oi/office360/restapi/service/ServiceSpringIntegrationTest.java
@@ -1,21 +1,24 @@
-//package oi.office360.restapi.service;
-//
-//import io.office360.restapi.spring.Office360PersistenceJpaConfig;
-//import io.office360.restapi.spring.Office360ServiceConfig;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.test.context.support.AnnotationConfigContextLoader;
-//
-//
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(classes = {Office360PersistenceJpaConfig.class, Office360ServiceConfig.class}, loader = AnnotationConfigContextLoader.class)
-//public class ServiceSpringIntegrationTest {
-//
-//    @Test
-//    public final void whenContextIsBootstrapped_thenOk() {
-//        //
-//    }
-//
-//}
+package oi.office360.restapi.service;
+
+import io.office360.restapi.spring.Office360PersistenceJpaConfig;
+import io.office360.restapi.spring.Office360ServiceConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {
+        Office360PersistenceJpaConfig.class,
+        Office360ServiceConfig.class},
+        loader = AnnotationConfigContextLoader.class)
+public class ServiceSpringIntegrationTest {
+
+    @Test
+    public final void whenContextIsBootstrapped_thenOk() {
+        //
+    }
+
+}
--- restapi/src/test/java/oi/office360/restapi/service/main/SecServiceIntegrationTest.java
+++ restapi/src/test/java/oi/office360/restapi/service/main/SecServiceIntegrationTest.java
@@ -1,24 +1,26 @@
-//package oi.office360.restapi.service.main;
-//
-//import io.office360.common.integration.service.AbstractServiceIntegrationTest;
-//import io.office360.common.persistence.model.INameableEntity;
-//import io.office360.restapi.spring.Office360ContextConfig;
-//import io.office360.restapi.spring.Office360ServiceConfig;
-//import org.junit.runner.RunWith;
-//import org.springframework.test.context.ContextConfiguration;
-//import org.springframework.test.context.TestPropertySource;
-//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-//import org.springframework.test.context.support.AnnotationConfigContextLoader;
-//
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@TestPropertySource(locations = "classpath:application.properties")
-//@ContextConfiguration(classes = {
-////        Office360PersistenceJpaConfig.class,
-//        Office360ServiceConfig.class,
-//        Office360ContextConfig.class
-//}, loader = AnnotationConfigContextLoader.class)
-//public abstract class SecServiceIntegrationTest<T extends INameableEntity> extends AbstractServiceIntegrationTest<T> {
-//
-//    //
-//
-//}
+package oi.office360.restapi.service.main;
+
+import io.office360.common.integration.service.AbstractServiceIntegrationTest;
+import io.office360.common.interfaces.INameableDto;
+import io.office360.restapi.spring.Office360ContextConfig;
+import io.office360.restapi.spring.Office360PersistenceJpaConfig;
+import io.office360.restapi.spring.Office360ServiceConfig;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@TestPropertySource(locations = "classpath:application.properties")
+@ContextConfiguration(classes = {
+        Office360PersistenceJpaConfig.class,
+        Office360ServiceConfig.class,
+        Office360ContextConfig.class
+}, loader = AnnotationConfigContextLoader.class)
+public abstract class SecServiceIntegrationTest<T extends INameableDto> extends
+        AbstractServiceIntegrationTest<T> {
+
+    //
+
+}
